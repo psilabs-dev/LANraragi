770fe6e580c83185a6ace5643c27c775ee92d004
psilabs <113860476+psilabs-dev@users.noreply.github.com>
2025-11-17 19:05:22 -0800

    Replace Redis lock with flock (yeahhh no) (#78)
    * switch from redis locks to flock
    
    * catch dies during rotation-time
    
    * typo
    
    * still needs a lock

--- lib/LANraragi/Utils/RotatingLog.pm
+++ lib/LANraragi/Utils/RotatingLog.pm
@@ -5,16 +5,11 @@ use warnings;
 use utf8;
 
 use Fcntl qw(:flock);
-use POSIX;
 use Compress::Zlib;
 use Config;
-use Time::HiRes;
 
 use Mojo::Base 'Mojo::Log';
-use Mojo::Util      qw(encode);
 use Mojo::File;
-use LANraragi::Utils::Redis qw(redis_decode);
-use LANraragi::Model::Config;
 
 use Exporter 'import';
 our @EXPORT_OK = qw(get_win32_fh);
@@ -32,6 +27,22 @@ has 'logfile';
 has retention_count     => sub { 0 + ($ENV{LRR_LOGROTATE_FILES} // 7) };        # max number of archived logfiles to retain for log rotation (defaults to 7 files).
 has max_rotation_size   => sub { 0 + ($ENV{LRR_LOGROTATE_SIZE} // 1048576) };   # max size of logfile (in bytes) before triggering rotation on next scan (defaults to 1 MB).
 has counter             => sub { 0 };
+has lockpath            => sub {
+    my $self = shift;
+    my $path = $self->path;
+    my $mf   = Mojo::File->new($path);
+    my $dir  = $mf->dirname;
+    my $real = $dir->realpath // $dir;
+    my $base = $mf->basename;
+    my $lockpath = Mojo::File->new($real, "$base.lock")->to_string;
+    return $lockpath;
+};
+has lockfh => sub {
+    my $self = shift;
+    my $lockpath = $self->lockpath;
+    open( my $fh, '>>', $lockpath ) or die "Could not open lockfile '$lockpath': $!";
+    return $fh;
+};
 
 # override: https://docs.mojolicious.org/Mojo/Log#handle
 has handle => sub {
@@ -58,33 +69,59 @@ sub append {
 
     $self->counter( $self->counter+1 );
 
+    my $path   = $self->path;
+    my $lockfh = $self->lockfh;
+
+    # Acquire shared lock to serialize with rotation EX lock.
+    flock( $lockfh, LOCK_SH ) or die "Failed to acquire shared log lock: $!";
+
+    # Refresh handle if inode changed due to rotation from another process
+    if ( IS_UNIX ) {
+        my $cached_inode = ( stat( $self->handle ) )[1];
+        my $path_inode   = ( stat( $path ) )[1];
+        if ( !defined $cached_inode || !defined $path_inode || $cached_inode != $path_inode ) {
+            open( my $fh, '>>', $path ) or die "Could not open logfile '$path': $!";
+            $self->handle($fh);
+        }
+    } else {
+        my $fh = get_win32_fh($path);
+        $self->handle($fh);
+    }
+
     # every 1k lines, check size of path for log rotation
     if ( $self->counter % 1000 == 0 ) {
 
-        my $path = $self->path;
-        if ( -s $path > $self->max_rotation_size && (my $logfile = $self->logfile) ) {
-            my $lock_name   = "log-rotate:$logfile";
-            my $redis       = LANraragi::Model::Config->get_redis_config;
-            my $lock        = $redis->set( $lock_name, 1, 'NX', 'EX', 10 );
-            my $rotation_error;
+        if ( -e $path && -s $path > $self->max_rotation_size ) {
+            # Upgrade: release SH then acquire EX, re-check, rotate
+            flock( $lockfh, LOCK_UN );
+            flock( $lockfh, LOCK_EX ) or die "Failed to acquire exclusive log lock: $!";
 
-            if ( $lock ) {
+            my $rotation_error;
+            if ( -e $path && -s $path > $self->max_rotation_size ) {
+                my $logfile = $self->logfile;
                 eval {
-                    rotate( $path, $self->retention_count );
+                    rotate_under_lock( $path, $logfile, $self->retention_count );
                     delete $self->{handle};
                     $self->handle;
+                    1;
+                } or do {
+                    my $lockpath = $self->lockpath;
+                    $rotation_error = "Failed to rotate logs during append-time under lock $lockpath: $@";
                 };
-                $rotation_error = $@;
-                $redis->del($lock_name);
+                die $rotation_error if $rotation_error;
             }
 
-            $redis->quit();
+            # Downgrade back to SH for the write
+            flock( $lockfh, LOCK_UN );
             die $rotation_error if $rotation_error;
+            flock( $lockfh, LOCK_SH ) or die "Failed to re-acquire shared log lock: $!";
         }
 
     }
 
-    return $self->SUPER::append($msg);
+    my $ret = $self->SUPER::append($msg);
+    flock( $lockfh, LOCK_UN );
+    return $ret;
 }
 
 # override: https://docs.mojolicious.org/Mojo/Log#new
@@ -96,80 +133,38 @@ sub new {
     my $path    = $self->path;
     my $logfile = $self->logfile;
 
-    # Logfile lock owners have exclusive ability to create a logfile.
-    # Non-owners may only append or wait for logfile availability.
-    my $lock_name   = "log-rotate:$logfile";
-    my $lock;
+    my $lockfh = $self->lockfh;
 
     if ( -e $path && -s $path > $self->max_rotation_size ) {
 
-        # Rotate log if it's > 1MB
-        my $redis       = LANraragi::Model::Config->get_redis_config;
-        $lock           = $redis->set( $lock_name, 1, 'NX', 'EX', 10 );
+        flock( $lockfh, LOCK_EX ) or die "Failed to acquire exclusive log lock: $!";
         my $rotation_error;
-
-        if ( $lock ) {
-            eval {
-                rotate( $path, $self->retention_count );
-                $self->handle;
-                1;
-            };
-
-            $rotation_error = $@;
-            $redis->del($lock_name);
-
-        }
-
-        $redis->quit();
+        eval {
+            rotate_under_lock( $path, $logfile, $self->retention_count );
+            $self->handle;
+            1;
+        } or do {
+            my $lockpath = $self->lockpath;
+            $rotation_error = "Failed to rotate logs during init-time under lock $lockpath: $@";
+        };
         die $rotation_error if $rotation_error;
+        flock( $lockfh, LOCK_UN );
 
     }
 
     # handle logpath existence cases.
     # case 1 (logfile exist):                   no action needed, just get the logfile handle
-    # case 2 (logfile DNE, lock not acquired):  wait 10s logfile to be available
-    # case 3 (logfile DNE, lock acquired):      create new logfile
+    # case 2 (logfile DNE):                      create new logfile under exclusive lock
     if ( !-e $path ) {
-        # handle cases where a logfile doesn't exist.
-
-        my $redis       = LANraragi::Model::Config->get_redis_config;
-        $lock           = $redis->set( $lock_name, 1, 'NX', 'EX', 10 );
-
+        flock( $lockfh, LOCK_EX ) or die "Failed to acquire exclusive log lock: $!";
         my $logfile_create_error;
-        if ( $lock ) {
-            # This happens during start of app (if no logfile exists).
-                say "Creating logfile $logfile.";
-            eval {
-                $self->handle;
-                1;
-            };
-
-            $logfile_create_error = $@;
-            $redis->del($lock_name);
-        } else {
-            # Another worker is rotating/creating the logfile.
-            my $tries       = 0;
-            my $acquired    = 0;
-            while ( $tries < 100 ) {
-                if ( !-e $path ) {
-                    Time::HiRes::sleep(0.1);
-                    $tries++;
-                } else {
-                    eval {
-                        $self->handle;
-                        1;
-                    };
-                    $logfile_create_error   = $@;
-                    $acquired               = 1;
-                    last;
-                }
-            }
-            if ( !$acquired ) {
-                $logfile_create_error = "Timed out waiting for logfile to be created: $path";
-            }
-        }
-
-        $redis->quit();
+        eval {
+            # Re-check inside lock in case another process created the file
+            $self->handle;
+            1;
+        };
+        $logfile_create_error = $@;
+        flock( $lockfh, LOCK_UN );
         die $logfile_create_error if $logfile_create_error;
 
     } else {
@@ -209,12 +204,36 @@ sub get_win32_fh {
     return *FH;
 }
 
+# Do log rotation (under Redis lock)
+sub rotate_under_lock {
+    my $logpath         = shift;
+    my $logfile         = shift;
+    my $retention_count = shift;
+
+    my $lock_name       = "log-rotate:$logfile";
+    my $redis           = LANraragi::Model::Config->get_redis_config;
+    my $lock            = $redis->set( $lock_name, 1, 'NX', 'EX', 10 );
+    my $rotation_error;
+
+    if ( $lock ) {
+        eval {
+            rotate( $logpath, $retention_count );
+        };
+
+        $rotation_error = $@;
+        $redis->del($lock_name);
+    }
+
+    $redis->quit();
+    die $rotation_error if $rotation_error;
+}
+
 # Do log rotation.
 sub rotate {
     my $logpath         = shift;
     my $retention_count = shift;
 
-        say "Rotating logpath $logpath";
+    say "Rotating logpath $logpath";
 
     # Based on Logfile::Rotate
     # Rotate existing logs
@@ -223,7 +242,7 @@ sub rotate {
         my $next = "$logpath.$i.gz";
         my $prev = "$logpath.$j.gz";
         if ( -r $prev && -f $prev ) {
-            rename( $prev, $next ) or die "error: rename failed: ($prev,$next)";
+            rename( $prev, $next ) or die "error: rename failed: ($prev,$next): $!";
         }
     }
 
