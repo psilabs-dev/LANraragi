b0fc4c7d67e0b95bd0fb8b8d2f45c5ea05cee072
psilabs-dev <113860476+psilabs-dev@users.noreply.github.com>
2025-11-18 12:02:26 -0800

    put lowlevel at bott

--- lib/LANraragi/Utils/RotatingLog.pm
+++ lib/LANraragi/Utils/RotatingLog.pm
@@ -148,30 +148,6 @@ sub new {
     return $self;
 }
 
-# Get perl file handler via Win32 native file handle of a logfile.
-# https://learn.microsoft.com/en-us/windows/win32/api/fileapi/nf-fileapi-createfilew
-# https://perldoc.perl.org/Win32API::File#CreateFile
-# https://perldoc.perl.org/Win32API::File#OsFHandleOpen
-sub get_win32_fh {
-    my $sPath    = shift;
-    my $uAccess  = Win32API::File::FILE_APPEND_DATA();
-    my $uShare   = Win32API::File::FILE_SHARE_READ()
-        | Win32API::File::FILE_SHARE_WRITE()
-        | Win32API::File::FILE_SHARE_DELETE();
-    my $pSecAttr = [];
-    my $uCreate  = Win32API::File::OPEN_ALWAYS();
-    my $uFlags   = 0;
-    my $hModel   = [];
-    my $h = Win32API::File::CreateFile( $sPath, $uAccess, $uShare, $pSecAttr, $uCreate, $uFlags, $hModel )
-        or die "CreateFile failed for $sPath; win32 says: $^E; errno: $!";
-
-    local *FH;
-
-    Win32API::File::OsFHandleOpen( *FH, $h, "w" ) or die "OsFHandleOpen failed for $sPath; $!";
-    binmode *FH, ':encoding(UTF-8)';
-    return *FH;
-}
-
 # Rotate logfiles if conditions met, otherwise do nothing.
 sub maybe_rotate {
     my $self    = shift;
@@ -337,6 +313,30 @@ sub get_handle {
     $fh = Mojo::File->new($path)->open('>>');
     $fh->binmode(':encoding(UTF-8)');
     return $fh;
+}
+
+# Get perl file handler via Win32 native file handle of a logfile.
+# https://learn.microsoft.com/en-us/windows/win32/api/fileapi/nf-fileapi-createfilew
+# https://perldoc.perl.org/Win32API::File#CreateFile
+# https://perldoc.perl.org/Win32API::File#OsFHandleOpen
+sub get_win32_fh {
+    my $sPath    = shift;
+    my $uAccess  = Win32API::File::FILE_APPEND_DATA();
+    my $uShare   = Win32API::File::FILE_SHARE_READ()
+        | Win32API::File::FILE_SHARE_WRITE()
+        | Win32API::File::FILE_SHARE_DELETE();
+    my $pSecAttr = [];
+    my $uCreate  = Win32API::File::OPEN_ALWAYS();
+    my $uFlags   = 0;
+    my $hModel   = [];
+    my $h = Win32API::File::CreateFile( $sPath, $uAccess, $uShare, $pSecAttr, $uCreate, $uFlags, $hModel )
+        or die "CreateFile failed for $sPath; win32 says: $^E; errno: $!";
+
+    local *FH;
+
+    Win32API::File::OsFHandleOpen( *FH, $h, "w" ) or die "OsFHandleOpen failed for $sPath; $!";
+    binmode *FH, ':encoding(UTF-8)';
+    return *FH;
 }
 
 1;
